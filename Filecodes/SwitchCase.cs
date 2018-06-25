c7191893ca33acf142718562cd2b9997d47b01fe
dangkhoagl <37831711+dangkhoagl@users.noreply.github.com>
2018-06-25 18:40:14 +0700

    Update images, Chapter 1 2 3 4

---
+++ Filecodes/SwitchCase.cs
@@ -0,0 +1,11 @@
+﻿public static int TinhY(int x)
+{
+    y = 0;
+    switch (x)
+    {
+        case 1: y += 4; break;
+        case 2: y *= 2; break;
+        default: y = y * y;
+    }
+    return y;
+}

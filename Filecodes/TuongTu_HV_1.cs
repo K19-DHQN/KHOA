5b6ba165ac44e9e56b4a46e2c9939d7aef5a68bb
dangkhoagl <37831711+dangkhoagl@users.noreply.github.com>
2018-07-20 19:22:29 +0700

    Add files via upload

--- Filecodes/TuongTu_HV_1.cs
+++ Filecodes/TuongTu_HV_1.cs
@@ -1,6 +1,6 @@
 using System;
 public class Program {
-    public static int TinhX(int x) {
+    public static int P1(int x) {
         if (x >= 0 && x <= 100)
             return x + 10;
         else

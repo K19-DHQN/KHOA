557f1dd36c31fd3f7dc64428816cc130e5ba8587
dangkhoagl <37831711+dangkhoagl@users.noreply.github.com>
2018-07-23 01:40:16 +0700

    Add files via upload

---
+++ Filecodes/PSE.cs
@@ -0,0 +1,5 @@
+﻿public int P_3 (int number) {
+    if(Program1(args) == Program2(args))
+        return 1;
+    return 0;
+}
---
+++ Filecodes/RS2.cs
@@ -0,0 +1,4 @@
+﻿public static int Y(string x) {
+	if (x == "ABC")	return 1	
+	return -1;
+}
